f8915b3e2c0aa5975a66595fbf098f16d15dedf5
rabab raza ali <robin.ali85@yahoo.com>
2019-07-12 23:31:03 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,4 +1,20 @@
 def first_challenge
-   epic_tragedy = {}
-   
+   epic_tragedy = {
+     :montague => {
+      :patriach => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+     }
+     :capulet  => {
+       :patriarch => {},
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends => 
+       
+       []
+      
+     }
+   }
+     
 end
